625a449afd84ea29e97b7763ce28c819e9c2b09d
Ben Kuhl <bkuhl@indatus.com>
2014-03-20 22:44:24 -0400

    Added license/copyright information

--- src/Indatus/Dispatcher/TableInterface.php
+++ src/Indatus/Dispatcher/TableInterface.php
@@ -1,11 +1,16 @@
 <?php
+
 /**
- * @author Ben Kuhl <bkuhl@indatus.com>
+ * This file is part of Dispatcher
+ *
+ * (c) Ben Kuhl <bkuhl@indatus.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Indatus\Dispatcher;
 
 use cli\table\Renderer;
-
 
 /**
  * Table class, only here so we can unit test our app
