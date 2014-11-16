3a0783d871e847b02627b95e04fbc4dce882d480
Ben Kuhl <bkuhl@indatus.com>
2014-11-16 12:51:16 -0500

    PHPCS updates

--- src/Indatus/Dispatcher/ConfigResolver.php
+++ src/Indatus/Dispatcher/ConfigResolver.php
@@ -9,7 +9,6 @@
  * file that was distributed with this source code.
  */
 
-use App;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Container\Container;
 use ReflectionException;
