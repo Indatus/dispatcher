5512ff87a2876009e14f26ae4ce5c17c66d2326d
Ben Kuhl <benkuhl@gmail.com>
2014-11-18 13:33:53 -0500

    Fixed busted test

--- tests/TestConfigResolver.php
+++ tests/TestConfigResolver.php
@@ -55,6 +55,6 @@ class TestConfigResolver extends TestCase
 
     public function testDriverCasing()
     {
-        $this->assertEquals('dateTime', $this->configResolver->getDriver());
+        $this->assertEquals('DateTime', $this->configResolver->getDriver());
     }
 }
